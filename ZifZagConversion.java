5d9bebad642a14f80f969fc369ed70ae7e0bcbd0
zelinzheng <zelin.zheng@gmail.com>
2014-04-19 21:42:13 -0700

    Update ZifZagConversion.java
    add to comment line. Those are needed if nRows<2 is not there in the condition

--- ZifZagConversion.java
+++ ZifZagConversion.java
@@ -1,27 +1,30 @@
 public class Solution {
     public String convert(String s, int nRows) {
-        if(s == null || s.length()<2 || nRows<2) return s;
-        
-        StringBuilder[] as = new StringBuilder[nRows];
-        for(int i=0;i<nRows;i++){
-            as[i] = new StringBuilder();
+        if(s==null || s.length() < 2 || nRows<2) return s;
+        StringBuilder[] sb = new StringBuilder[nRows];
+        for(int i=0; i<sb.length; i++) {
+            sb[i] = new StringBuilder();
         }
-        int j=-1;
-        boolean increase = true;
+        
+        boolean down = true;
+        int j=0;
         for(int i=0; i<s.length(); i++) {
-            if(increase) {
+            sb[j].append(s.charAt(i));
+            if(down) {
+                //if(j < nRows-1) j++;
                 j++;
-                if(j==nRows-1) increase = false;
+                if(j== nRows -1) down = false;
             } else {
+                //if(j>0) j--;
                 j--;
-                if(j==0) increase = true;
+                if(j==0) down = true;
             }
-            as[j].append(s.charAt(i));
-        }
-        for(int i=1; i<nRows;i++) {
-            as[0].append(as[i]);
         }
         
-        return as[0].toString();
+        for(int i=1; i<nRows; i++) {
+            sb[0].append(sb[i]);
+        }
+        
+        return sb[0].toString();
     }
 }
